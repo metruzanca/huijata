e53a309f006fc8995035b79cf6fd10bfe551510b
Sam Zanca <samuele@zanca.dev>
2026-08-14 08:35:10 -0400

    feat: help screen shots config and save path

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/metruzanca/huijata/internal/config"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +14,37 @@ var rootCmd = &cobra.Command{
 	SilenceUsage: true,
 }
 
+// updateHelpLong fills in the root command's help with the resolved locations
+// of huijata's config file and the Noita save folder, so `huijata --help`
+// always shows where things live.
+func updateHelpLong() {
+	cfgPath, err := config.Path()
+	if err != nil {
+		cfgPath = "(could not determine)"
+	}
+
+	savePath := ""
+	cfg, err := config.Load()
+	if err == nil && cfg != nil {
+		savePath = cfg.SavePath()
+	} else {
+		savePath = config.GuessGamePath()
+		if savePath != "" {
+			savePath += string(os.PathSeparator) + "save00"
+		} else {
+			savePath = "(not configured; run `huijata init`)"
+		}
+	}
+
+	rootCmd.Long = fmt.Sprintf(`Huijata is a tool for the game noita.
+
+Config file: %s
+Save folder: %s
+`, cfgPath, savePath)
+}
+
 func Execute() {
+	updateHelpLong()
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
